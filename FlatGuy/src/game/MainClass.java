c40bf4a9cf85faf15546bd9dcc79b4d98fc6bb48
106389 <106389@10.113.10.160>
2016-12-20 17:41:38 -0500

    Alright we've got some stuff

--- FlatGuy/src/game/MainClass.java
+++ FlatGuy/src/game/MainClass.java
@@ -1,24 +1,68 @@
 package game;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
 import javax.swing.*;
 
-public class MainClass extends JApplet {
+public class MainClass extends JApplet implements ActionListener {
+
+	Player mainDude = new Player();
+	Timer t;
 
 	public void init() {
+
 		Container c = getContentPane();
 		c.setBackground(Color.white);
-		c.setSize(1000, 1000);
+		c.setSize(700, 500);
+		t = new Timer(1000, this);
+		t.start();
+	}
+
+	public void keyPressed(KeyEvent e) {
+		int k = e.getKeyCode();
+
+		switch (k) {
+		case KeyEvent.VK_RIGHT:
+			mainDude.setRightButton(true);
+			break;
+		case KeyEvent.VK_LEFT:
+			mainDude.setLeftButton(true);
+			break;
+		case KeyEvent.VK_UP:
+			mainDude.jump();
+			break;
+		}
 
 	}
-	
-	
-	//paints junk
+
+	public void keyReleased(KeyEvent e) {
+		int k = e.getKeyCode();
+		switch (k) {
+		case KeyEvent.VK_LEFT:
+			mainDude.setLeftButton(false);
+			break;
+		case KeyEvent.VK_RIGHT:
+			mainDude.setRightButton(false);
+			break;
+
+		}
+
+	}
+
 	public void paint(Graphics g) {
 		super.paint(g);
-
 		g.setColor(Color.BLACK);
 		g.fillRect(60, 70, 50, 50);
+		g.fillRect(mainDude.getX(), mainDude.getY(), mainDude.getWidth(), mainDude.getHeight());
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		mainDude.timePassed();
+		repaint();
 	}
 
 }
--- FlatGuy/src/game/Player.java
+++ FlatGuy/src/game/Player.java
@@ -1,51 +1,102 @@
 package game;
 
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
 public class Player {
 
 	// positions on the different axises
 	private int x;
 	private int y;
 	private int z;
+    private int jumpPower;
 	
-	//width and height of the hitbox
+	// width and height of the hitbox
 	private final int width;
 	private final int height;
-	
-	//velocities automagically initialized to 0
-	double xVel;
-	double yVel;
-	double zVel;
-	
-	//controls
-	boolean isRightButtonPressed;
-	boolean isLeftButtonPressed;
-	boolean isForwardButtonPressed;
-	boolean isBackwardButtonPressed;
 
+	// velocities automagically initialized to 0
+	private double xVel;
+	private double yVel;
+	private double zVel;
+
+	// controls
+	private boolean isRightButtonPressed;
+	private boolean isLeftButtonPressed;
+	private boolean isForwardButtonPressed;
+	private boolean isBackwardButtonPressed;
+	private boolean isJumpButtonPressed;
+	private boolean hasJumped;
+	
+	
+	
+	public void timePassed(){
+		//alters velocities
+		if (isRightButtonPressed){
+			xVel++;
+		}
+		if (isLeftButtonPressed){
+			xVel--;
+		}
+		if(isJumpButtonPressed){
+			yVel -= jumpPower;
+		}
+		//gravity
+		yVel--;
+		
+		//move stuff
+		x += xVel;
+		y += yVel;
+		
+	}
 	
 	public Player() {
-		width = 100;
-		height = 100;
-	}	
-	
-	public void setRightButton(boolean k){
+		width = 50;
+		height = 50;
+		x = 100;
+		y = 200;
+		jumpPower = 20;
+	}
+
+	public void setRightButton(boolean k) {
 		isRightButtonPressed = k;
 	}
-	
-	public void setLeftButton(boolean k){
+
+	public void setLeftButton(boolean k) {
 		isLeftButtonPressed = k;
 	}
-	
-	public void setForwardButton(boolean k){
+
+	public void setForwardButton(boolean k) {
 		isForwardButtonPressed = k;
 	}
-	
-	public void setBackwardButton(boolean k){
+
+	public void setBackwardButton(boolean k) {
 		isBackwardButtonPressed = k;
 	}
+
+	public void jump() {
+		isJumpButtonPressed = true;
+	}
+
+	public int getX() {
+		return x;
+	}
 	
-	public int getZ(){
+	public int getY() {
+		return y;
+	}
+	
+	public int getZ() {
 		return z;
 	}
 	
+	public int getWidth(){
+		return width;
+	}
+	
+	public int getHeight(){
+		return height;
+	}
+
 }
