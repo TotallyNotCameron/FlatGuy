48ad0b22c0e0df2b92b34debe74ea8de476da0cf
100735 <100735@10.113.85.180>
2016-12-20 15:47:04 -0500

    update

--- FlatGuy/src/game/MainFrame.java
+++ FlatGuy/src/game/MainFrame.java
@@ -1,14 +1,39 @@
 package game;
 
-import javax.swing.JApplet;
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class MainFrame extends JApplet {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainFrame extends JPanel {
+	
+	private static int width = 600;
+	private static int height = 400;
 
 	/**
 	 * Create the applet.
 	 */
 	public MainFrame() {
 
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.RED);
+		g.fillRect(width / 2, height / 2, 200, 400);
+	}
+	
+	public static void main(String[] args) {
+		MainFrame example = new MainFrame();
+		
+		JFrame jf = new JFrame();
+		jf.setTitle("FlatGuy");
+		jf.setSize(width, height);
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.add(example);
+		
 	}
 
 }
